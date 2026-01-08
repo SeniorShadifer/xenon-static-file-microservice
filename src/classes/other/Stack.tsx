a030778e5e8323a890a829324dbbb0b01522e67e
SeniorShadifer <dicordeg@mail.ru>
2026-01-08 13:40:30 +0300

    Updated Stack class.

--- src/classes/other/Stack.tsx
+++ src/classes/other/Stack.tsx
@@ -6,4 +6,24 @@ export class Stack {
     this.pages = pages;
     this.current_page_index = current_page_index;
   }
+
+  public setPage(element: any, index: number) {
+    if (this.pages.length > index) {
+      this.pages[index] = element;
+    } else {
+      this.pages.push(element);
+    }
+  }
+
+  public setCurrentPage(element: any) {
+    this.setPage(element, this.current_page_index);
+  }
+
+  public getPage(index: number): any {
+    return this.pages.at(index);
+  }
+
+  public getCurrentPage(): any {
+    return this.getPage(this.current_page_index);
+  }
 }
